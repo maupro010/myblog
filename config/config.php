6849ece9e5825e8877b71f5b6255dc015fa3b943
Phuc <phuc51mta@gmail.com>
2022-06-01 11:02:37 +0700

    add config.php

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+//Admin information
+const ADMIN_ACCOUNT  = 'admin';
+const ADMIN_PASS = '123456';
+
+//Database information
+const DB_HOST = 'localhost';
+const DB_USER = 'root';
+const DB_PASS = 'Phuc97@#';
+const DB_NAME = 'pdoposts';
+const DB_TABLE = 'posts';
